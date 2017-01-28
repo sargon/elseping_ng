27cf8f8a6c0926cd8f5d265c6c9fa9041d421770
Daniel Ehlers <danielehlers@mindeye.net>
2017-01-28 14:45:01 +0100

    tasks: Calculate next repeat time

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+import datetime
+
 class Task(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
+    # Last time task has been completed
+    last_complete = models.DateTimeField()
     # Current repeating factor
     repeat_factor = models.PositiveIntegerField()
     # Maximal repeating factor 
     max_repeat_factor = models.PositiveIntegerField()
+
+    def get_next_repeat(self):
+        timedelta = datetime.timedelta(days=self.repeat_factor)
+        return self.last_complete + timedelta
