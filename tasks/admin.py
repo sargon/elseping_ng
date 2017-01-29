c3fb0ba6818e6dc434733cbf77b54988be3ea89b
Cybe <cybe@toppoint.de>
2017-01-29 16:41:03 +0100

    admin-interface dynamics

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Task
 
-# Register your models here.
-admin.site.register(Task)
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'last_complete',
+                    'next_repeat', 'repeat_factor', 'max_repeat_factor')
+    list_display_links = ('name',)
+    list_filter = ('next_repeat', 'last_complete')
+    search_fields = ('name', 'description')
+    class Meta:
+        model = Task
+
+
+admin.site.register(Task, TaskModelAdmin)
